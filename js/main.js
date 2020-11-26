55bff6bf09576390a00ab62c60107702753b83e1
sandretto <alepet.petr1638@yandex.ru>
2020-11-26 04:16:36 +0300

    Add slider (desktop)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+    speed: 900,
+    effect: 'fade'
+  });
