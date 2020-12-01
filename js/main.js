4fb723d8c15911f2c4cc162dea1aab3e299ff5bc
sandretto <alepet.petr1638@yandex.ru>
2020-12-01 22:47:59 +0300

    Add JavaScript API

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ var mySwiper = new Swiper('.swiper-container', {
       pageUpDown: true
     }
   });
+
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [59.763561, 30.355990],
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([59.763561, 30.355990], {
+            iconCaption: 'Хилтон Санкт-Петербург Экспофорум'
+        }, {
+            preset: 'islands#greenDotIconWithCaption'
+        }));
+}
