83b2cec37f6896e93e2f5f583e29a069a5f552b3
sandretto <alepet.petr1638@yandex.ru>
2020-12-09 20:23:27 +0300

    Add form validation

--- js/main.js
+++ js/main.js
@@ -71,6 +71,8 @@ $(document).ready(function () {
   var modalButton = $('[data-toggle=modal]');
   var modalOverlay = $('.modal__overlay');
   var modalDialog = $('.modal__dialog');
+  var modalInput = $('.input');
+  var label = $('label');
 
   modalButton.on('click', openModal);
 
@@ -88,17 +90,62 @@ $(document).ready(function () {
 
   closeButton.on('click', closeModal);
 
-  function closeModal(event) {
-    event.preventDefault();
+  function hideModal(modalOverlay, modalDialog) {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
+    $('.modal__form').validate().resetForm();
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    hideModal(modalOverlay, modalDialog);
   }
 
   $(document).keydown(function(event) {
     var code = event.keyCode || event.which;
     if (code == 27) {
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
+      hideModal(modalOverlay, modalDialog);
     }
   });
+
+  //form validation
+  $.validator.addMethod( 'mobileRU', function( phone_number, element ) {
+    var ruPhone_number = phone_number.replace( /\(|\)|\s+|-/g, "" );
+    return this.optional( element ) || ruPhone_number.length > 9 && /^((\+7|7|8)+([0-9]){10})$/.test( ruPhone_number );
+  }, "Your mobile number must be in the format of +79999999999" );
+
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: {
+          required: true,
+          mobileRU: true
+        },
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name should not be shorter than 2 symbols"
+        },
+        phone: {
+          required: "We need your mobile phone to contact you",
+          phone: "Your mobile number must be in the format of +79999999999"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        }
+      }
+    });
+  });
 });
+
